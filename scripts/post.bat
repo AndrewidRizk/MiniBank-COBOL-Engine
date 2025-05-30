4d85abbbbb7d47565c0134c0e4a1d3c8502143b3
Andrew Rizk <97995173+AndrewidRizk@users.noreply.github.com>
2025-05-30 01:29:44 -0400

    Fixed endpoints

--- scripts/post.bat
+++ scripts/post.bat
@@ -1,3 +1,6 @@
 curl -X POST http://127.0.0.1:8000/api/deposit/ ^
      -H "Content-Type: application/json" ^
      -d "{\"account_id\": \"000000004\", \"amount\": 100500}"
+
+curl -X GET http://127.0.0.1:8000/api/account/000000004/balance/ ^
+     -H "Content-Type: application/json" ^ 
